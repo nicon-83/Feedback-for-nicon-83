a33e4e42d930435234df695414a12e7105136f87
Vladimir <nicon-83@mail.ru>
2020-02-09 00:43:52 +0500

    first test ready

--- client.js
+++ client.js
@@ -1,7 +1,17 @@
-const { axios } = require("./fakeBackend/mock");
+const {axios} = require("./fakeBackend/mock");
 
 const getFeedbackByProductViewData = async (product, actualize = false) => {
 
+	try {
+		const response = await axios.get(`/feedback?product=${product}`);
+		return response.data;
+	} catch (error) {
+		if (error.status === 404) return {
+			"message": "Такого продукта не существует"
+		};
+		return error;
+	}
+
 };
 
-module.exports = { getFeedbackByProductViewData };
+module.exports = {getFeedbackByProductViewData};
